0039d1d3a222409aa5e3b72a7b026c0488d6ccbc
uqdalter <uqdalter@CA-SINT-4D5HW22.uq.edu.au>
2017-08-18 11:23:45 +1000

    Removed URLENV from healthcheck test as it bencame obsolete

--- src/test/java/stepDefinition/LoginSteps.java
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -4,11 +4,10 @@ import org.junit.Assert;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.Before;
-
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import pageclasses.DashboardPage;
 import pageclasses.LandingPage;
 import util.CustomFunctions;
